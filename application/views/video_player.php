3271dfa676d08efc69c8a269eb0712aafe1bc6cc
Julio Foulquié <jfoulquie@gmail.com>
2012-03-13 00:52:44 +0100

    Clean Codeigniter 2.1.0 and controller and view for the application.

---
+++ application/views/video_player.php
@@ -0,0 +1,13 @@
+<!-- <!DOCTYPE html> -->
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta charset="utf-8">
+	<link rel="icon" href="http://elfaryesdios.info/favicon.png" type="image/png">
+	<meta name="viewport" content="width=device-width; height=device-height;">
+	<title><?php echo $title ?></title>
+</head>
+<body marginwidth="0" marginheight="0">
+	<embed width="100%" height="100%" name="plugin" src=<?php echo $url; ?> type="application/x-shockwave-flash">
+</body>
+</html>
