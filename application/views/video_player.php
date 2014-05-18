de649287d3604554983d67b4f223e5dda496bb4e
Julio Foulquié <jfoulquie@gmail.com>
2014-05-17 20:55:53 -0400

    Add Piwik stats JS to the header.

--- application/views/video_player.php
+++ application/views/video_player.php
@@ -10,14 +10,31 @@ echo '<meta name="description" content="' . $description . '">';
 echo '<meta property="og:title" content="' . $title . '">';
 echo '<meta property="og:image" content="https://i4.ytimg.com/vi/' . $video_id . '/hqdefault.jpg">';
 echo '<meta property="og:description" content="' . $description . '">'
-
 ?>
+
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://dezwartepoet.nl/piwik/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 4]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
+    g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="http://dezwartepoet.nl/piwik/piwik.php?idsite=4" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
+
+
 <meta property="og:type" content="video">
 <meta property="og:video:type" content="application/x-shockwave-flash">
 <meta property="og:site_name" content="El Fary Es Dios">
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width; height=device-height;">
-	<title><?php echo $title ?></title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width; height=device-height;">
+<title><?php echo $title ?></title>
 </head>
 <body marginwidth="0" marginheight="0">
 	<embed width="100%" height="100%" name="plugin" src=<?php echo $url; ?> type="application/x-shockwave-flash">
